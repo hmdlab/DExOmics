07f0bb5bdcf70761ce30bbdc5a7f16f740149521
Yi9898 <yihuang9898@gmail.com>
2025-04-25 11:10:22 +0900

    txt.gz download

--- data_download/load_regulator.sh
+++ data_download/load_regulator.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-for txtfile in *.txt; do
-    folder="${txtfile%.txt}"
+for gzfile in *.txt.gz; do
+    folder="${gzfile%.txt.gz}"
     mkdir -p "../data/$folder"
 
-    while read url; do
+    zcat "$gzfile" | while read url; do
         wget -P "../data/$folder" "$url"
-    done < "$txtfile"
+    done
 
-    for gzfile in "../data/$folder"/*.gz; do
-        if [[ -f "$gzfile" ]]; then
-            gunzip -f "$gzfile"
+    for file in "../data/$folder"/*.gz; do
+        if [[ -f "$file" ]]; then
+            gunzip -f "$file"
         fi
     done
 done
